798e5a5e3189c2acbb2e8fb32f508904283a3680
mabud <pavelalam401@gmail.com>
2025-03-01 19:03:58 +0530

    Add PrimaryButton component for consistent button styling

---
+++ lib/app/components/buttons.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:turfit/app/constants/colors.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final String tile;
+  final void Function() onPressed;
+
+  const PrimaryButton({super.key, required this.tile, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        onPressed();
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: MealAIColors.darkPrimary,
+        foregroundColor: MealAIColors.lightPrimary,
+        minimumSize: const Size(double.infinity, 50),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      child: Text(tile),
+    );
+  }
+}
