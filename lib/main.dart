1b66aabc4d2e6ba90836e333b30f3ec94abd58ec
mabud <pavelalam401@gmail.com>
2025-03-01 19:04:10 +0530

    Integrate MultiProvider for theme management and update navigation to OnboardingHome

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
 import 'package:turfit/app/models/Auth/user_repo.dart';
 import 'package:turfit/app/modules/Auth/blocs/authentication_bloc/authentication_bloc.dart';
 import 'package:turfit/app/modules/Auth/blocs/sign_in_bloc/sign_in_bloc.dart';
-import 'package:turfit/app/modules/Auth/views/welcome_screen.dart';
 import 'package:turfit/app/modules/DashBoard/view/home_screen.dart';
+import 'package:turfit/app/modules/Onboarding/views/onboarding_home.dart';
+import 'package:turfit/app/providers/theme_provider.dart';
 import 'package:turfit/app/repo/firebase_user_repo.dart';
 import 'package:turfit/app/utility/simple_bloc_observer.dart';
 
@@ -13,7 +16,14 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   Bloc.observer = SimpleBlocObserver();
-  runApp(MyApp(FirebaseUserRepo()));
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+      ],
+      child: MyApp(FirebaseUserRepo()),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -34,22 +44,29 @@ class MyAppView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase Auth',
-      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
-        builder: (context, state) {
-          if (state.status == AuthenticationStatus.authenticated) {
-            return BlocProvider(
-              create: (context) => SignInBloc(
-                  userRepository:
-                      context.read<AuthenticationBloc>().userRepository),
-              child: const HomeScreen(),
-            );
-          } else {
-            return const WelcomeScreen();
-          }
-        },
-      ),
-    );
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    return Sizer(builder: (context, orientation, deviceType) {
+      return MaterialApp(
+        title: 'Firebase Auth',
+        debugShowCheckedModeBanner: true,
+        theme: themeProvider.lightTheme,
+        darkTheme: themeProvider.darkTheme,
+        themeMode: themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+        home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+          builder: (context, state) {
+            if (state.status == AuthenticationStatus.authenticated) {
+              return BlocProvider(
+                create: (context) => SignInBloc(
+                    userRepository:
+                        context.read<AuthenticationBloc>().userRepository),
+                child: const HomeScreen(),
+              );
+            } else {
+              return const OnboardingHome();
+            }
+          },
+        ),
+      );
+    });
   }
 }
