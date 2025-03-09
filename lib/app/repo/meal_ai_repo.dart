fa3e599ca1b2dea685ea0abbf2d774ef70bb6c52
mabud <pavelalam401@gmail.com>
2025-03-10 00:34:47 +0530

    Add AiRepository class for fetching nutrition data from API

--- lib/app/repo/meal_ai_repo.dart
+++ lib/app/repo/meal_ai_repo.dart
@@ -1,1 +1,24 @@
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+import 'package:turfit/app/constants/urls.dart';
+
+import '../models/AI/nutrition_input.dart';
+import '../models/AI/nutrition_output.dart';
+
+class AiRepository {
+  Future<NutritionOutput> getNutritionData(
+      NutritionInputQuery inputQuery) async {
+    final response = await http.post(
+      Uri.parse(BaseUrl.baseUrl + ApiPath.getNutritionData),
+      body: jsonEncode(inputQuery.toJson()),
+      headers: {"Content-Type": "application/json"},
+    );
+
+    if (response.statusCode == 200) {
+      return NutritionOutput.fromJson(json.decode(response.body));
+    } else {
+      throw Exception("Failed to fetch data");
+    }
+  }
+}
