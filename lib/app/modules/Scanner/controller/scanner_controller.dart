0779afc2e3b9baf6fb92b7d7bf709de97b1274a8
mabud <pavelalam401@gmail.com>
2025-03-26 23:42:03 +0530

    Refactor ScannerController to enhance state management and improve nutrition record handling

--- lib/app/modules/Scanner/controller/scanner_controller.dart
+++ lib/app/modules/Scanner/controller/scanner_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:get/get.dart';
+import 'package:turfit/app/constants/enums.dart';
 import 'package:turfit/app/models/AI/nutrition_input.dart';
 import 'package:turfit/app/models/AI/nutrition_output.dart';
 import 'package:turfit/app/models/AI/nutrition_record.dart';
@@ -12,82 +13,167 @@ import 'package:turfit/app/utility/image_utility.dart';
 import 'package:turfit/app/utility/registry_service.dart';
 
 class ScannerController extends GetxController {
-  DailyNutritionRecords? exisitingNutritionRecords;
+  // Variables for state management
   List<NutritionRecord> dailyRecords = [];
   DateTime selectedDate = DateTime.now();
-  var isLoading = false.obs;
+  bool isLoading = false;
+  DailyNutritionRecords? existingNutritionRecords;
 
-  Future<NutritionOutput> getNutritionData(
-      NutritionInputQuery inputData, String userId) async {
-    AiRepository aiRepository = serviceLocator<AiRepository>();
+  @override
+  void onInit() {
+    super.onInit();
+    print("📌 ScannerController initialized");
+  }
 
-    try {
-      NutritionOutput nutritionOutput =
-          await aiRepository.getNutritionData(inputData);
-      return nutritionOutput;
-    } catch (e) {
-      print("🔥 [API Error] $e");
-      throw Exception("❌ Something went wrong: $e");
-    }
+  void addRecord(NutritionRecord record) {
+    dailyRecords.add(record);
+    update();
+  }
+
+  void removeRecord(NutritionRecord record) {
+    dailyRecords.remove(record);
+    update();
+  }
+
+  void updateRecord(NutritionRecord record) {
+    final index =
+        dailyRecords.indexWhere((r) => r.recordTime == record.recordTime);
+    dailyRecords[index] = record;
+    update();
   }
 
   Future<void> processNutritionQueryRequest(
       String userId, File image, ScanMode scanMode) async {
-    isLoading.value = true;
-    NutritionRecordRepo nutritionRecordRepo =
-        serviceLocator<NutritionRecordRepo>();
     try {
-      File resizedFile = await ImageUtility.downscaleImage(
+      print(
+          "--- processNutritionQueryRequest --- UserId: $userId --- ScanMode: $scanMode");
+
+      final nutritionRecordRepo = serviceLocator<NutritionRecordRepo>();
+      final storageService = serviceLocator<StorageService>();
+      final aiRepository = serviceLocator<AiRepository>();
+
+      DateTime time = DateTime.now();
+
+      NutritionRecord nutritionRecord = NutritionRecord(
+        recordTime: time,
+        nutritionInputQuery: NutritionInputQuery(
+          imageUrl: "",
+          scanMode: scanMode,
+          imageData: "",
+          imageFilePath: image.path,
+        ),
+        processingStatus: ProcessingStatus.PROCESSING,
+      );
+
+      addRecord(nutritionRecord);
+
+      ///First We will downscale image
+      final resizedFile = await ImageUtility.downscaleImage(
         image.path,
         scale: ImageScale.large_2048,
       );
 
+      print("--- Let's get the Base64 ----");
+
+      ///Convert image to base64 and upload
       final base64String =
           await ImageUtility.convertImageToBase64(resizedFile.path);
-      StorageService storageService = serviceLocator<StorageService>();
 
-      // Await the uploadImage method and handle the nullable return type
-      String? imageUrl = await storageService.uploadImage(resizedFile);
+      print("The Image Base64 is ${base64String.length}");
+
+      print("--- Lets get the nutrition data from Meal AI backend");
+
+      NutritionOutput rawNutritionData = await aiRepository.getNutritionData(
+        NutritionInputQuery(
+          imageUrl: "",
+          scanMode: scanMode,
+          imageData: base64String,
+          // imageFilePath: image.path,
+        ),
+      );
+
+      print("We got the rawNutritionData: ${rawNutritionData.status}");
+
+      print("--- Now let's get the Image url");
+
+      ///Upload the imageurl in the firebase
+      final imageUrl = await storageService.uploadImage(resizedFile);
+
+      print("We got the image URL: $imageUrl");
 
       if (imageUrl == null) {
-        throw Exception("Failed to upload image.");
+        print("Failed to upload image.");
       }
 
-      NutritionInputQuery inputData = NutritionInputQuery(
-        imageUrl: imageUrl,
+      ///Prepare input data for AI (with imageData)
+      final inputData = NutritionInputQuery(
+        imageUrl: imageUrl!,
         scanMode: scanMode,
-        imageData: base64String,
+        imageData: base64String, // Include imageData for API request
+        imageFilePath: image.path,
       );
 
-      NutritionOutput nutritionOutput =
-          await getNutritionData(inputData, userId);
+      String dailyRecordID = nutritionRecordRepo.getRecordId(time);
 
-      NutritionRecord nutritionRecord = NutritionRecord(
-        nutritionOutput: nutritionOutput,
-        recordTime: DateTime.now(),
+      print("The dailyRecordID is $dailyRecordID");
+
+      updateRecord(NutritionRecord(
+        nutritionOutput: rawNutritionData,
+        recordTime: time,
         nutritionInputQuery: inputData,
+        processingStatus: ProcessingStatus.COMPLETED,
+      ));
+
+      DailyNutritionRecords dailyNutritionRecords = DailyNutritionRecords(
+        dailyRecords: dailyRecords,
+        recordDate: time,
+        recordId: dailyRecordID,
       );
 
-      String recordId = nutritionRecordRepo.getRecordId(DateTime.now());
-
-      List<NutritionRecord> dailyRecords =
-          exisitingNutritionRecords?.dailyRecords ?? [];
-
-      DailyNutritionRecords? dailyNutritionRecords = DailyNutritionRecords(
-        dailyRecords: [...dailyRecords, nutritionRecord],
-        recordDate: DateTime.now(),
-        recordId: recordId,
-      );
-
-      await nutritionRecordRepo.saveNutritionData(
+      final status = await nutritionRecordRepo.saveNutritionData(
           dailyNutritionRecords, userId);
+
+      existingNutritionRecords = dailyNutritionRecords;
 
       update();
     } catch (e) {
       print("🔥 [API Error] $e");
+    }
+  }
+
+  Future<void> getRecordByDate(String userId, DateTime selectedDate) async {
+    print("🔍 Fetching nutrition records for user: $userId on $selectedDate");
+
+    try {
+      // Set loading state and update UI
+      // isLoading = true;
+      // update();
+
+      final nutritionRecordRepo = serviceLocator<NutritionRecordRepo>();
+
+      // Fetch records
+      final records =
+          await nutritionRecordRepo.getNutritionData(userId, selectedDate);
+
+      // Update existing records and daily records
+      existingNutritionRecords = records;
+      dailyRecords = records.dailyRecords;
+
+      print("✅ Successfully retrieved records");
+      update();
+    } catch (e) {
+      print("🔥 [API Error] $e");
+
+      // Clear records on error
+      dailyRecords.clear();
+      existingNutritionRecords = null;
+      update();
+
       throw Exception("❌ Something went wrong: $e");
     } finally {
-      isLoading.value = false;
+      // Always set loading to false
+      isLoading = false;
+      update();
     }
   }
 }
