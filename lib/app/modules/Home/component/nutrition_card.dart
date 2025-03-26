43ee0ffa05483de659f96d6271af2046dcd5738e
mabud <pavelalam401@gmail.com>
2025-03-26 23:42:22 +0530

    Refactor NutritionCard to use NutritionRecord and display processing status with image handling

--- lib/app/modules/Home/component/nutrition_card.dart
+++ lib/app/modules/Home/component/nutrition_card.dart
@@ -1,12 +1,42 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:turfit/app/models/AI/nutrition_output.dart';
+import 'package:sizer/sizer.dart';
+import 'package:turfit/app/constants/enums.dart';
+import 'package:turfit/app/models/AI/nutrition_record.dart';
 
 class NutritionCard extends StatelessWidget {
-  final NutritionOutput nutritionOutput;
-  const NutritionCard({super.key, required this.nutritionOutput});
+  final NutritionRecord nutritionRecord;
+  const NutritionCard({super.key, required this.nutritionRecord});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      child: nutritionRecord.processingStatus == ProcessingStatus.PROCESSING
+          ? Row(
+              children: [
+                // nutritionRecord.processingStatus == ProcessingStatus.PROCESSING
+                //     ? Image.file(
+                //         File(nutritionRecord.nutritionInputQuery!.imageFilePath!))
+                // : CachedNetworkImage(
+                //     imageUrl: nutritionRecord.nutritionInputQuery!.imageUrl!,
+                //     placeholder: (context, url) => CircularProgressIndicator(),
+                //     width: 20.w,
+                //     height: 10.h,
+                //   ),
+
+                Image.file(
+                  File(nutritionRecord.nutritionInputQuery!.imageFilePath!),
+                  width: 20.w,
+                  height: 10.h,
+                ),
+
+                Text("Processing..."),
+              ],
+            )
+          : Row(
+              children: [],
+            ),
+    );
   }
 }
