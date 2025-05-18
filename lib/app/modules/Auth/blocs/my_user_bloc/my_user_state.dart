904b774127c64a0a6fe1e56dffeaeb7307febe57
Mabud <mabud.alam@featsclub.com>
2025-05-18 13:16:34 +0530

    Refactor UserBloc and UserEvent classes to improve user data loading and error handling; update UserState to include error management.

--- lib/app/modules/Auth/blocs/my_user_bloc/my_user_state.dart
+++ lib/app/modules/Auth/blocs/my_user_bloc/my_user_state.dart
@@ -1,10 +1,31 @@
-part of 'my_user_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:turfit/app/models/Auth/user.dart';
 
-sealed class MyUserState extends Equatable {
-  const MyUserState();
-  
+abstract class UserState extends Equatable {
+  const UserState();
+
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
-final class MyUserInitial extends MyUserState {}
+class UserInitial extends UserState {}
+
+class UserLoading extends UserState {}
+
+class UserLoaded extends UserState {
+  final UserModel userModel;
+
+  const UserLoaded(this.userModel);
+
+  @override
+  List<Object?> get props => [userModel];
+}
+
+class UserError extends UserState {
+  final String message;
+
+  const UserError(this.message);
+
+  @override
+  List<Object?> get props => [message];
+}
