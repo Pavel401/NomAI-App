b4fb8c72fc7011b34cce44d8fded49e097d734b0
Mabud <mabud.alam@featsclub.com>
2025-06-17 00:35:48 +0530

    Fix base URL and update API path format for consistency

--- lib/app/constants/urls.dart
+++ lib/app/constants/urls.dart
@@ -1,8 +1,8 @@
 class BaseUrl {
   // static const String baseUrl = 'https://nutrix.up.railway.app/';
-  static const String baseUrl = 'http://10.0.2.2:8000/'; // Instead of 127.0.0.1
+  static const String baseUrl = 'http://10.0.2.2:8080/'; // Instead of 127.0.0.1
 }
 
 class ApiPath {
-  static const String getNutritionData = "nutrition/get";
+  static const String getNutritionData = "/nutrition/get";
 }
