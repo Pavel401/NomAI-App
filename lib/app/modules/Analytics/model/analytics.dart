9a151fc99c4cf68e9e721cac488f39940a1c5397
Mabud <mabud.alam@featsclub.com>
2025-09-25 21:07:01 +0530

    feat: Enhance MonthlyAnalytics and DailyAnalytics models with additional fields and improved JSON serialization

--- lib/app/modules/Analytics/model/analytics.dart
+++ lib/app/modules/Analytics/model/analytics.dart
@@ -1,41 +1,66 @@
 class MonthlyAnalytics {
-  List<DailyAnalytics> dailyAnalytics;
+  final List<DailyAnalytics> dailyAnalytics;
 
-  DateTime lastModified;
+  final String overAllMonthlySummary;
+
+  final DateTime lastModified;
 
   MonthlyAnalytics({
     required this.dailyAnalytics,
     required this.lastModified,
+    this.overAllMonthlySummary = '',
   });
 
-  toJson() {
+  Map<String, dynamic> toJson() {
     return {
       'dailyAnalytics': dailyAnalytics.map((e) => e.toJson()).toList(),
       'lastModified': lastModified.toIso8601String(),
+      'overAllMonthlySummary': overAllMonthlySummary,
     };
   }
 
   factory MonthlyAnalytics.fromJson(Map<String, dynamic> json) {
+    final dynamic listAny = json['dailyAnalytics'];
+    final daily = (listAny is List)
+        ? listAny
+            .whereType<Map>()
+            .map((e) => DailyAnalytics.fromJson(Map<String, dynamic>.from(e)))
+            .toList()
+        : <DailyAnalytics>[];
+
+    final dynamic lm = json['lastModified'];
+    DateTime lastMod;
+    if (lm is String) {
+      lastMod = DateTime.tryParse(lm) ?? DateTime.now();
+    } else if (lm is DateTime) {
+      lastMod = lm;
+    } else {
+      lastMod = DateTime.now();
+    }
+
+    final String summary = (json['overAllMonthlySummary'] as String?) ?? '';
+
     return MonthlyAnalytics(
-      dailyAnalytics: (json['dailyAnalytics'] as List)
-          .map((e) => DailyAnalytics.fromJson(e))
-          .toList(),
-      lastModified: DateTime.parse(json['lastModified']),
+      dailyAnalytics: daily,
+      overAllMonthlySummary: summary,
+      lastModified: lastMod,
     );
   }
 }
 
 class DailyAnalytics {
-  DateTime date;
-  int totalCalories;
-  int totalProtein;
-  int totalFat;
-  int totalCarbs;
-  int mealCount;
+  final DateTime date;
+  final int totalCalories;
+  final int totalProtein;
+  final int totalFat;
+  final int totalCarbs;
+  final int mealCount;
 
-  int totalCaloriesBurned;
+  final int totalCaloriesBurned;
 
-  int waterIntake;
+  final int waterIntake;
+
+  final String? overAllSummary;
 
   DailyAnalytics({
     required this.date,
@@ -46,9 +71,10 @@ class DailyAnalytics {
     this.mealCount = 0,
     this.totalCaloriesBurned = 0,
     this.waterIntake = 0,
+    this.overAllSummary,
   });
 
-  toJson() {
+  Map<String, dynamic> toJson() {
     return {
       'date': date.toIso8601String(),
       'totalCalories': totalCalories,
@@ -58,19 +84,33 @@ class DailyAnalytics {
       'mealCount': mealCount,
       'totalCaloriesBurned': totalCaloriesBurned,
       'waterIntake': waterIntake,
+      'overAllSummary': overAllSummary,
     };
   }
 
   factory DailyAnalytics.fromJson(Map<String, dynamic> json) {
+    final dynamic d = json['date'];
+    DateTime parsedDate;
+    if (d is String) {
+      parsedDate = DateTime.tryParse(d) ?? DateTime.now();
+    } else if (d is DateTime) {
+      parsedDate = d;
+    } else {
+      parsedDate = DateTime.now();
+    }
+
+    int _asInt(dynamic v) => v is num ? v.toInt() : (int.tryParse('$v') ?? 0);
+
     return DailyAnalytics(
-      date: DateTime.parse(json['date']),
-      totalCalories: json['totalCalories'] ?? 0,
-      totalProtein: json['totalProtein'] ?? 0,
-      totalFat: json['totalFat'] ?? 0,
-      totalCarbs: json['totalCarbs'] ?? 0,
-      mealCount: json['mealCount'] ?? 0,
-      totalCaloriesBurned: json['totalCaloriesBurned'] ?? 0,
-      waterIntake: json['waterIntake'] ?? 0,
+      date: parsedDate,
+      totalCalories: _asInt(json['totalCalories']),
+      totalProtein: _asInt(json['totalProtein']),
+      totalFat: _asInt(json['totalFat']),
+      totalCarbs: _asInt(json['totalCarbs']),
+      mealCount: _asInt(json['mealCount']),
+      totalCaloriesBurned: _asInt(json['totalCaloriesBurned']),
+      waterIntake: _asInt(json['waterIntake']),
+      overAllSummary: json['overAllSummary'] as String?,
     );
   }
 }
